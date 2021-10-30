d8f6cdfa55e8cc46def04d3763bda546de615fae
ShanibTeciz <smshanibburney@tecizeverything.com>
2021-10-30 18:00:51 +0500

    changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { fetchUsers, addUsers, updateUser, deleteUsers, updatePoints} = require('../Controllers/userController');
+const { fetchUsers, addUsers, updateUser, deleteUsers, updatePoints} = require('../controllers/userController');
 
 const router = express.Router();
 
